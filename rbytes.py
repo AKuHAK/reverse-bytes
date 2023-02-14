b93ceb0a67af2a177353887e12b69441666ab926
AKuHAK <621640+AKuHAK@users.noreply.github.com>
2023-02-14 10:08:08 +0200

    Switch to mx4sio byte reversal

--- rbytes.py
+++ rbytes.py
@@ -19,12 +19,19 @@ def main(arguments):
     parser = argparse.ArgumentParser(
         description=__doc__,
         formatter_class=argparse.RawDescriptionHelpFormatter)
-    parser.add_argument('infile', help="Input file", type=argparse.FileType('rb'))
+    parser.add_argument('infile', help="Input file",
+                        type=argparse.FileType('rb'))
     parser.add_argument('-o', '--outfile', help="Output file",
                         default=sys.stdout, type=argparse.FileType('wb'))
 
     args = parser.parse_args(arguments)
-    args.outfile.write(args.infile.read()[::-1])
+    byte_list = bytearray(args.infile.read())
+    for i in range(len(byte_list)):
+        byte_list[i] = int(bin(byte_list[i])[2:].zfill(8)[::-1], 2)
+
+    reversed_contents = bytes(byte_list)
+    args.outfile.write(reversed_contents)
+
 
 if __name__ == '__main__':
     sys.exit(main(sys.argv[1:]))
